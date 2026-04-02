4298f6236207be68aba4ab7b356494ce218d7ac8
phamviethai1213 <phamviethai1213@gmail.com>
2026-04-02 19:02:31 +0700

    deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // CLI: npm install express body-parser --save
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -24,6 +25,18 @@ app.use((req, res, next) => {
 app.use('/api/admin', require('./api/admin.js'));
 // customer routes
 app.use('/api/customer', require('./api/customer.js'));
+
+// deployment
+// '/admin' serve the files at client-admin/build/* as static files
+app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.get('/admin/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'));
+});
+// '/' serve the files at client-customer/build/* as static files
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+});
 
 // start server AFTER all routes/middleware registered
 app.listen(PORT, () => {
